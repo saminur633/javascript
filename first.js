fafba1d62687e600e009e79adec4822ee0fcc082
saminur633 <rahmansaminur51@gmail.com>
2024-01-21 16:36:48 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,9 @@
+var sam = 5;
+var temperature = 38;
+// string
+var person = "saminur Rahman";
+var location = 'Mirpur';
+// 
+var serious = true;
+var issingle = false;
+console.log(person, location);
---
+++ variable-name.js
@@ -0,0 +1,9 @@
+var usernameAddress = "dhaka";
+var a = 10;
+var b = 20;
+
+console.log(a+b);
+var a = 500;
+var b = 50;
+var totalcost= a/b;
+console.log(totalcost);
